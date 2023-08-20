4c65a31b876022f61378edb150de0fc12be3f509
Sharknia <53037836+Sharknia@users.noreply.github.com>
2023-08-20 23:01:12 +0900

    Revert "tags 일때에는 갯수를 이름 뒤에 붙이고 정렬을 갯수로"
    This reverts commit 6d388a8a209c0e960a294b6de47ed875b631d3b8.

--- src/libs/utils/notion/getAllSelectItemsFromPosts.ts
+++ src/libs/utils/notion/getAllSelectItemsFromPosts.ts
@@ -7,7 +7,6 @@ export function getAllSelectItemsFromPosts(
   const selectedPosts = posts.filter((post) => post?.[key])
   const items = [...selectedPosts.map((p) => p[key]).flat()]
   const itemObj: { [itemName: string]: number } = {}
-  
   items.forEach((item) => {
     if (!item) return
     if (item in itemObj) {
@@ -16,18 +15,14 @@ export function getAllSelectItemsFromPosts(
       itemObj[item] = 1
     }
   })
-  //tags 일때에는 갯수를 이름 뒤에 붙이고 정렬을 갯수로 함
-  const sortedEntries: Array<[string, number]> = key === "tags"
-  ? Object.entries(itemObj)
-      .sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
-      .map(([key, val]) => [`${key} (${val})`, val])
-  : Object.entries(itemObj)
-      .sort((a, b) => a[0].localeCompare(b[0]));
-
-  const sortedItemObj = sortedEntries.reduce((acc, [key, val]) => {
-    acc[key as string] = val
-    return acc
-  }, {} as { [itemName: string]: number })
+  
+  // itemObj를 item name으로 정렬
+  const sortedItemObj = Object.entries(itemObj)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .reduce((acc, [key, val]) => {
+      acc[key] = val
+      return acc
+    }, {} as { [itemName: string]: number })
 
   return sortedItemObj
 }
